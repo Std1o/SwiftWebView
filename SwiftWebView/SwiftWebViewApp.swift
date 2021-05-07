cbd9939d2f233a1072fb7d3e1d721df9c88dfbd3
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-07 19:31:57 +0300

    Initial commit

---
+++ SwiftWebView/SwiftWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWebViewApp.swift
+//  SwiftWebView
+//
+//  Created by Илья on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
